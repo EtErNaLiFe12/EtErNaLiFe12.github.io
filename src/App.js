5f20d51cf90d943755b9bb36285365e14636556a
wnsqja8888 <wnsqja8888@gmail.com>
2022-06-06 18:28:13 +0900

    page test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import MarkdownRenderer from './components/MarkdownRenderer.tsx';
 function App() {
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>사이트 만들기 성공!</div>
+        <MarkdownRenderer/>
       </header>
     </div>
   );
