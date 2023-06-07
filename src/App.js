a390356cfd554fcf47fd06d7546dbf103d3f10f1
wnsqja8888 <wnsqja8888@gmail.com>
2023-06-07 22:29:32 +0900

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import './App.css';
-import MarkdownRenderer from './components/MarkdownRenderer.tsx';
+import "./App.css";
+import MarkdownRenderer from "./components/MarkdownRenderer.tsx";
 function App() {
-
-  
-
   return (
     <div className="App">
       <header className="App-header">
-        <div>사이트 만들기 성공!</div>
-        <MarkdownRenderer/>
+        <div>사이트 만들기 성공!!!!</div>
+        <MarkdownRenderer />
       </header>
     </div>
   );
